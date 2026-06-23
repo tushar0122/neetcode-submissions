4ac1f17996607e0168e5801474bf5faf28dcebba
Tushar Agrawal <tusharagrawal8484@gmail.com>
2026-06-23 10:27:02 +0530

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int change(int amount, int[] coins) {
+        int dp[] = new int[amount+1];
+        int n = coins.length;
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i]<=j){
+                    dp[j]+=dp[j-coins[i]];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
+
+// dp[amount]=sum of dp[amount-coins[i]] for i 0 to n
