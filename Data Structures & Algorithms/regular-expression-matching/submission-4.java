6953c68fa0e2ccc5b8563487e1e866d637f7839f
Tushar Agrawal <tusharagrawal8484@gmail.com>
2026-06-25 09:03:13 +0530

    Add: regular-expression-matching - submission-4

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-4.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isEqual(String s, String p, int i, int j){
+        return p.charAt(j)=='.'||p.charAt(j)==s.charAt(i);
+    }
+
+    public boolean rec(String s, String p, int i, int j, int dp[][]){
+        if(i==-1&&j==-1){
+            return true;
+        }
+        if(i<0&&j>=0){
+            if(p.charAt(j)=='*')return rec(s, p, i, j-2, dp);
+            return false;
+        }
+        if(j<0||i<0){
+            return false;
+        }
+        if(dp[i][j]!=0){
+            return dp[i][j]==1?true:false;
+        }
+        if(p.charAt(j)!='*'){
+            boolean r1 =isEqual(s, p, i, j);
+            boolean r2 = rec(s, p, i-1, j-1, dp);
+            dp[i][j]=r1&&r2?1:2;
+            return r1&&r2;
+        }
+        else{
+            boolean r1 =isEqual(s, p, i, j-1)&&(rec(s, p, i-1, j, dp)||rec(s, p, i-1, j-2, dp));
+            boolean r2 = rec(s, p, i, j-2, dp);
+            var r = r1||r2;
+            dp[i][j]=r?1:2;
+            return r;
+        }
+    }
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        int dp[][] = new int[n][m];
+        rec(s, p, n-1, m-1, dp);
+        return dp[n-1][m-1]==1?true:false;
+    }
+}
